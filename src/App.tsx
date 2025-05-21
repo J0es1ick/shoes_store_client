e8ef1fdd5c02d2a077a3ed7d12a4ea11479814b4
J0es1ick <mrjoeslick@gmail.com>
2025-05-21 11:02:33 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./App.css";
+
+const App: React.FC = () => (
+  <>
+    <h1>Hello World!</h1>
+    <h2>Hello</h2>
+  </>
+);
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createRoot } from "react-dom/client";
+
+const container = document.getElementById("root");
+const root = createRoot(container!);
 root.render(
   <React.StrictMode>
     <App />
